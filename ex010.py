4204dd1f8c24f798a51f09fca67c1f9b3e8737c9
Jonathan Alvarenga <alvarenga.jonathan@outlook.com>
2023-06-08 21:58:34 -0300

    update

--- ex010.py
+++ ex010.py
@@ -1,6 +1,12 @@
 # Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar .
-# considere US$1,00 = R$3,27.
+# considere US$1,00 = R$4,92.
 
-reais = float(input('Quanto de dinheiro você tem na carteira? : '))
-dollars = reais // 3.27
-print('Você consegue comprar {} dólares'.format(dollars))
+reais = float(input('Quanto dinheiro você tem na carteira? : R$ '))
+dollars = reais / 4.92
+euro = reais / 5.31
+iene = reais / 0.035
+peso = reais / 0.020
+print('Com R${:.2f} , você pode comprar US${:.2f} dólares'.format(reais,dollars))
+print('Com R${:.2f} , você pode comprar JPY${:.2f} ienes'.format(reais, iene))
+print('Com R${:.2f} , você pode comprar EUR${:.2f} euros'.format(reais,euro))
+print('Com R${:.2f} você pode comprar ARS${:.2f} pesos argentinos'.format(reais,peso))
