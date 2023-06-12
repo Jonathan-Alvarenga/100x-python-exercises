c82647e258f03b535c915afc1024488a350ce8be
Jonathan Alvarenga <alvarenga.jonathan@outlook.com>
2023-06-12 18:13:44 -0300

    commit

--- ex028.py
+++ ex028.py
@@ -2,3 +2,13 @@
 # e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
 # O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
+from random import randint
+pc = randint(0,5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente advinhar...')
+print('-=-' * 20)
+player = int(input('Em que número eu pensei ? :'))
+if pc == player:
+    print('PARABÉNS! Você me venceu!')
+else:
+    print('GANHEI! Eu pensei no número {} e não no {}.'.format(pc,player))
