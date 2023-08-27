414d26b275b7c20b25cb13330011a2944c43529b
Jonathan Alvarenga <alvarenga.jonathan@outlook.com>
2023-08-27 13:59:48 -0300

    up

--- ex69.py
+++ ex69.py
@@ -9,4 +9,21 @@
 #B) quantos homens foram cadastrados.
 
 #C) quantas mulheres tem menos de 20 anos.
-
+maiorIdade = menorIdade = homens = mulherNova = 0
+while True:
+    idade = int(input('Digite sua idade :'))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Digite seu sexo: [M/F]')).strip().upper()[0]
+    if idade >= 18:
+        maiorIdade += 1
+    if sexo == 'M':
+        homens += 1
+    if sexo == 'F' and idade < 20:
+        mulherNova += 1
+    op = ' '
+    while op not in 'SN':
+        op = str(input('Quer continuar ? [S/N]')).strip().upper()[0]
+    if op == 'N':
+        break
+print(f'Foram computados {maiorIdade} pessoas maiores de 18 anos , sendo {homens} homens e {mulherNova} mulheres com menos de 20 anos.')
