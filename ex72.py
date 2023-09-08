34828ebcf7a6f2c07a00b65185787e7c59ce61af
Jonathan Alvarenga <alvarenga.jonathan@outlook.com>
2023-09-07 21:49:24 -0300

    up

--- ex72.py
+++ ex72.py
@@ -1,3 +1,10 @@
+#Exercício Python 72: Crie um programa que tenha uma dupla totalmente preenchida
+# com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+
+
+
 ex = ("Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze", "Treze", "Catorze", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove", "Vinte")
 while True:
     num = int(input('Digite um número entre 0 e 20:'))
--- ex73.py
+++ ex73.py
@@ -1,3 +1,15 @@
+#Exercício Python 73: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
+
+#a) Os 5 primeiros times.
+
+#b) Os últimos 4 colocados.
+
+#c) Times em ordem alfabética.
+
+#d) Em que posição está o time da Chapecoense.
+
+
+
 times = ('Botafogo', 'Palmeiras', 'Grêmio', 'Flamengo', 'Fluminense', 'Red Bull Bragantino',
          'Athletico-PR', 'Fortaleza', 'Atlético-MG', 'Cuiabá' ,'São Paulo', 'Cruzeiro', 'Corinthians',
          'Internacional' ,'Goiás' ,'Bahia', 'Santos', 'Vasco', 'América-MG', 'Coritiba')
